59f0d7d918d4d64bfdaca7587fff677b4bea12d4
Kris011 <94460102+Kris011@users.noreply.github.com>
2021-12-15 17:09:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print("""
     \033[1;32m[\033[0m+\033[1;32m]\033[0m github: https://github.com/Kris011
     \033[1;31m[\033[0m-\033[1;31m]\033[0m Ctrl C stop server
 """)
-file = """<h1>Bienvenido a Felix py</h1><br><p>Creado por Hector Diaz</p><br><h3>!Si quieres tu servidor modifica el archivo main.py en la linea 16 donde dice file</h3><br><li>Gracias</li><a href="https://github.com/Kris011">Apoyame en mi github</a>
+file = """<h1>Bienvenido a Felix py</h1><br><h3>!Si quieres tu servidor modifica el archivo main.py en la linea 16 donde dice file</h3><br><li>Gracias</li><a href="https://github.com/Kris011">Apoyame en mi github</a>
 """
 app = Flask(__name__)
 @app.route("/")
